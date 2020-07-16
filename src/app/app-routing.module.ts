d83346c0218592f6b191e7637dfc669acc6a37f8
BVaChen <ben00129@gmail.com>
2020-07-16 17:52:06 -0400

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SolicitationFieldsComponent } from './solicitation-fields/solicitation-fields.component';
+import { SubmittedPageComponent } from './submitted-page/submitted-page.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'solicitationPage', component: SolicitationFieldsComponent},
+  {path: 'submittedPage', component: SubmittedPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
