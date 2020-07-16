d75ff69da2d6adcae9c70185d3bc17bb79407ff8
BVaChen <ben00129@gmail.com>
2020-07-16 17:48:24 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SolicitationFieldsComponent } from './solicitation-fields/solicitation-fields.component';
+import { SubmittedPageComponent } from './submitted-page/submitted-page.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     SolicitationFieldsComponent,
+    SubmittedPageComponent
   ],
   imports: [
     BrowserModule,
